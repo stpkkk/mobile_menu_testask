d4140ff8a7ae2ba51550545b455bc9073fe34953
Igor Stepanov <stipyk1309@gmail.com>
2023-06-22 00:49:36 +0300

    icons index.ts with imports create

---
+++ public/icons/index.ts
@@ -0,0 +1,27 @@
+import { ArrowDownIcon } from "./ArrowDownIcon";
+import { ArrowLeftIcon } from "./ArrowLeftIcon";
+import { ArrowRightIcon } from "./ArrowRightIcon";
+import { BurgerMenuIcon } from "./BurgerMenuIcon";
+import { CheckIcon } from "./CheckIcon";
+import { CloseIcon } from "./CloseIcon";
+import { FlagBelarus } from "./FlagBelarus";
+import { FlagKazakhstan } from "./FlagKazakhstan";
+import { FlagNetherlands } from "./FlagNetherlands";
+import { FlagRussia } from "./FlagRussia";
+import { FlagTurkey } from "./FlagTurkey";
+import { FlagUSA } from "./FlagUSA";
+
+export {
+  ArrowDownIcon,
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  BurgerMenuIcon,
+  CheckIcon,
+  CloseIcon,
+  FlagBelarus,
+  FlagKazakhstan,
+  FlagNetherlands,
+  FlagRussia,
+  FlagTurkey,
+  FlagUSA,
+};
