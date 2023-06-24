89a7c14dc903a4eb621672923330bc78bcf16116
Igor Stepanov <stipyk1309@gmail.com>
2023-06-25 02:49:15 +0300

    TertiaryMenu fix scroll

--- components/TertiaryMenu.tsx
+++ components/TertiaryMenu.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import BackButton from "./BackButton";
 import { TertiaryMenuItem } from "@types";
@@ -9,32 +9,42 @@ type Props = {
 
 const TertiaryMenu: React.FC<Props> = ({ items }) => {
   const { t } = useTranslation();
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const menuDiv = menuRef.current;
+    if (menuDiv && menuDiv.scrollHeight > window.innerHeight) {
+      menuDiv.classList.add("overflow-y-scroll");
+      menuDiv.classList.add("border-t-[1px]");
+      menuDiv.classList.add("border-grayLine");
+      menuDiv.classList.add("opacity-70");
+    } else {
+      menuDiv?.classList.remove("overflow-y-scroll");
+    }
+  }, []);
 
   return (
     <>
       <BackButton />
-      <div className="border-b-[1px] border-grayLine opacity-70" />
-      <ul
-        className={`${
-          items.length > 8 && "overflow-y-scroll h-[calc(100vh-10rem)]"
-        }`}
-      >
-        {items.map((item: TertiaryMenuItem) => (
-          <li
-            className="cursor-pointer py-3 pl-5 pr-10 hover:bg-blue-300"
-            key={item.id}
-          >
-            <>
-              <div className="font-medium text-[16px] leading-6">
-                {t(item.title)}
-              </div>
-              <div className="text-[12px] leading-5 text-gray">
-                {t(item.content)}
-              </div>
-            </>
-          </li>
-        ))}
-      </ul>
+      <div className="flex flex-col" ref={menuRef}>
+        <ul className="h-[calc(100vh-10.5rem)]">
+          {items.map((item: TertiaryMenuItem) => (
+            <li
+              className="cursor-pointer py-3 pl-5 pr-10 hover:bg-blue-300"
+              key={item.id}
+            >
+              <>
+                <div className="font-medium text-[16px] leading-6">
+                  {t(item.title)}
+                </div>
+                <div className="text-[12px] leading-5 text-gray">
+                  {t(item.content)}
+                </div>
+              </>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 };
